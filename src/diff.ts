9ce89035453b3d2cb0906aca62ba8f5762f89e83
Stian Haga <hagastian@gmail.com>
2023-04-01 13:03:19 +0200

    feat: use new context in diff

--- src/diff.ts
+++ src/diff.ts
@@ -2,57 +2,58 @@ import { webkit, firefox, chromium, Browser } from "@playwright/test";
 import { PNG } from "pngjs";
 import pixelmatch from "pixelmatch";
 import fs from "fs";
+import { ChromascopeContext } from "./chromascope-context";
 
 interface DiffOptions {
   verbose: boolean;
   runId: string;
 }
 
-export const diff = async (link: string, options: DiffOptions) => {
-  await diffWebkit(link, options);
-  await diffFirefox(link, options);
-  await diffChromium(link, options);
-  await diffChromiumToWebkit(options);
+export const diff = async (link: string, ctx: ChromascopeContext) => {
+  await diffWebkit(link, ctx);
+  await diffFirefox(link, ctx);
+  await diffChromium(link, ctx);
+  await diffChromiumToWebkit(ctx);
   return true;
 };
 
-const diffWebkit = async (link: string, options: DiffOptions) => {
+const diffWebkit = async (link: string, ctx: ChromascopeContext) => {
   const browser = await webkit.launch();
-  await runBrowserDiff(link, browser, options);
+  await runBrowserDiff(link, browser, ctx);
 };
 
-const diffFirefox = async (link: string, options: DiffOptions) => {
+const diffFirefox = async (link: string, ctx: ChromascopeContext) => {
   const browser = await firefox.launch();
-  await runBrowserDiff(link, browser, options);
+  await runBrowserDiff(link, browser, ctx);
 };
 
-const diffChromium = async (link: string, options: DiffOptions) => {
+const diffChromium = async (link: string, ctx: ChromascopeContext) => {
   const browser = await chromium.launch();
-  await runBrowserDiff(link, browser, options);
+  await runBrowserDiff(link, browser, ctx);
 };
 
 const runBrowserDiff = async (
   link: string,
   browser: Browser,
-  options: DiffOptions
+  ctx: ChromascopeContext
 ) => {
   const context = await browser.newContext();
   const type = browser.browserType();
   const name = type.name();
   const page = await context.newPage();
   await page.goto(link);
-  const path = `runs/${options.runId}/${name}.png`;
-  if (options.verbose) {
+  const path = `runs/${ctx.runId}/${name}.png`;
+  if (ctx.options.verbose) {
     console.log(`Saving ${name} screenshot to ${path}`);
   }
   await page.screenshot({ path });
 };
 
-const diffChromiumToWebkit = (options: DiffOptions) => {
+const diffChromiumToWebkit = (ctx: ChromascopeContext) => {
   const baseFolder = `chromascope-runs`;
-  const chromiumPath = `${baseFolder}/${options.runId}/chromium.png`;
-  const webkitPath = `${baseFolder}/${options.runId}/webkit.png`;
-  const diffPath = `${baseFolder}/${options.runId}/diff.png`;
+  const chromiumPath = `${baseFolder}/${ctx.runId}/chromium.png`;
+  const webkitPath = `${baseFolder}/${ctx.runId}/webkit.png`;
+  const diffPath = `${baseFolder}/${ctx.runId}/diff.png`;
 
   const chromiumPng = PNG.sync.read(fs.readFileSync(chromiumPath));
   const webkitPng = PNG.sync.read(fs.readFileSync(webkitPath));
