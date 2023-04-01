44f2a3f40eed6fb79bf235ef2ef298fadc24909a
Stian Haga <hagastian@gmail.com>
2023-04-01 13:01:43 +0200

    feat: add logger class

---
+++ src/logger.ts
@@ -0,0 +1,24 @@
+export type LoggerOptions = typeof Logger.prototype.options | undefined;
+
+class Logger {
+  options = { verbose: false };
+
+  constructor(options: Partial<LoggerOptions> = undefined) {
+    this.setOptions(options);
+  }
+  setOptions(options: Partial<LoggerOptions>) {
+    this.options = { ...this.options, ...options };
+  }
+  log(...args: any[]) {
+    console.log(...args);
+  }
+
+  debug(...args: any[]) {
+    if (this.options.verbose) {
+      console.log(...args);
+    }
+  }
+}
+const logger = new Logger();
+
+export default logger;
