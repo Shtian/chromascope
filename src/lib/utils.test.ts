0010ff6a0e0e6808cb165b741cdbf21e014a8bd8
Stian Haga <hagastian@gmail.com>
2023-04-02 21:40:52 +0200

    feat: add cookie parsing utility function

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,7 +1,7 @@
-import { isUrl } from "./utils";
+import { isUrl, parseCookieOptions } from "./utils";
 import { describe, expect, it } from "vitest";
 
-describe("utils", () => {
+describe("isUrl", () => {
   it("should allow localhost urls", () => {
     const url = "http://localhost:3000";
     expect(isUrl(url)).toBe(true);
@@ -33,5 +33,43 @@ describe("utils", () => {
   it("should not allow urls without a domain", () => {
     const url = "https://";
     expect(isUrl(url)).toBe(false);
+  });
+});
+
+describe("parseCookieOptions", () => {
+  const url = "https://example.com";
+  it("should parse cookies", () => {
+    const cookies = "foo=bar;bar=baz";
+    expect(parseCookieOptions(cookies, url)).toEqual([
+      { name: "foo", value: "bar", url },
+      { name: "bar", value: "baz", url },
+    ]);
+  });
+  it("should parse single cookie", () => {
+    const cookies = "foo=bar";
+    expect(parseCookieOptions(cookies, url)).toEqual([{ name: "foo", value: "bar", url }]);
+  });
+  it("should parse cookies with spaces", () => {
+    const cookies = "foo = bar; bar = baz";
+    expect(parseCookieOptions(cookies, url)).toEqual([
+      { name: "foo", value: "bar", url },
+      { name: "bar", value: "baz", url },
+    ]);
+  });
+  it("should parse an empty string without throwing", () => {
+    const cookies = "";
+    expect(parseCookieOptions(cookies, url)).toEqual([]);
+  });
+  it("should parse a value with an equals sign", () => {
+    const cookies =
+      "CookieConsent={stamp:%27WxrO46wiM4iF3wmVF00zO/M3+azPktEBeauYQCSr2t2oLxf8s+u42A==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:2%2Cutc:1677699545253%2Cregion:%27no%27}";
+    expect(parseCookieOptions(cookies, url)).toEqual([
+      {
+        name: "CookieConsent",
+        value:
+          "{stamp:%27WxrO46wiM4iF3wmVF00zO/M3+azPktEBeauYQCSr2t2oLxf8s+u42A==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:2%2Cutc:1677699545253%2Cregion:%27no%27}",
+        url,
+      },
+    ]);
   });
 });
