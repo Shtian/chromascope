757097b9ac400f1bb40d115d31556fcd7599d9f8
Stian Haga <hagastian@gmail.com>
2023-04-02 21:40:52 +0200

    feat: add cookie parsing utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,15 @@ const urlRegex =
   /^(?:(?:https?|ftp):\/\/)?(?:localhost|\S+(?:\.[^\s.]+)+|\[?[0-9a-fA-F:]+\]?)(?::\d+)?(?:\/[\w#!:.?+=&%@!\-\/]*)?(?:\?[\w&=]+)?$/i;
 
 export const isUrl = (url: string) => urlRegex.test(url);
+
+export const parseCookieOptions = (cookies: string, url: string) => {
+  const parsedCookies: Array<{ name: string; value: string; url: string }> = [];
+  cookies.split(";").forEach((cookie) => {
+    if (!cookie) return;
+    const indexOfEquals = cookie.indexOf("=");
+    const key = cookie.slice(0, indexOfEquals);
+    const values = cookie.slice(indexOfEquals + 1);
+    parsedCookies.push({ name: key.trim(), value: values.trim(), url });
+  });
+  return parsedCookies;
+};
